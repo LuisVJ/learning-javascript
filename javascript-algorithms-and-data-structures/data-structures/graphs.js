d0dcfdfc2c383f33142705685c3100e87fd6402f
Luis Vidal <43793069+LuisVJ@users.noreply.github.com>
2021-07-04 00:04:52 +0200

    Complete Graph DFS and BFS implementation

--- javascript-algorithms-and-data-structures/data-structures/graphs.js
+++ javascript-algorithms-and-data-structures/data-structures/graphs.js
@@ -34,23 +34,104 @@ class Graph {
         }
         delete this.adjacencyList[vertex];
     }
+
+    DFS(start){
+        let result = [];
+        let visited = {};
+        let adjacencyList = this.adjacencyList;
+        function recursiveDFS(vertex){
+            result.push(vertex);
+            visited[vertex] = true;
+            for (let v of adjacencyList[vertex]) {
+                if (!visited[v]){
+                    recursiveDFS(v);
+                } 
+            };
+        }
+        recursiveDFS(start)
+
+        return result;
+    }
+
+    DFS_iterative(start){
+        const stack = [start];
+        const result = [];
+        const visited = {};
+        let currentVertex;
+
+        visited[start] = true;
+        while(stack.length){
+            currentVertex = stack.pop();
+            result.push(currentVertex);
+
+            this.adjacencyList[currentVertex].forEach(neigbor => {
+                if (!visited[neigbor]){
+                    visited[neigbor] = true;
+                    stack.push(neigbor);
+                }
+            });
+        }
+        return result;
+    }
+
+    BFS(start){
+        let queue = [start];
+        let result = [];
+        let visited = {};
+        let currentVertex;
+
+        visited[start] = true;
+        while(queue.length){
+            currentVertex = queue.shift();
+            result.push(currentVertex);
+
+            this.adjacencyList[currentVertex].forEach(neigbor => {
+                if (!visited[neigbor]){
+                    visited[neigbor] = true;
+                    queue.push(neigbor);
+                }
+            });
+        }
+        return result;
+    }
 }
 
 
 g = new Graph();
 
-g.addVertex("Tokyo");
-g.addVertex("Madrid");
-g.addVertex("Madrid");
-g.addVertex("Leon");
-g.addVertex("Pucela");
+// g.addVertex("Tokyo");
+// g.addVertex("Madrid");
+// g.addVertex("Madrid");
+// g.addVertex("Leon");
+// g.addVertex("Pucela");
 
-g.addEdge("Tokyo", "Madrid");
-g.addEdge("Pucela", "Madrid");
-g.addEdge("Leon", "Madrid");
+// g.addEdge("Tokyo", "Madrid");
+// g.addEdge("Pucela", "Madrid");
+// g.addEdge("Leon", "Madrid");
 
-console.log(g);
+// console.log(g);
 
-g.removeVertex("Madrid");
+// g.removeVertex("Madrid");
 
-console.log(g);
+// console.log(g);
+
+g.addVertex("A")
+g.addVertex("B")
+g.addVertex("C")
+g.addVertex("D")
+g.addVertex("E")
+g.addVertex("F")
+
+g.addEdge("A", "B")
+g.addEdge("A", "C")
+g.addEdge("B", "D")
+g.addEdge("C", "E")
+g.addEdge("D", "E")
+g.addEdge("D", "F")
+g.addEdge("E", "F")
+
+console.log(g)
+
+console.log(g.DFS("A"))
+console.log(g.DFS_iterative("A"))
+console.log(g.BFS("A"))
